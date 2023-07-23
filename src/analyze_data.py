d84f0abf405912e8ffa4cf569877f7677a5cf458
satishreddyseelam <140163832+satishreddyseelam@users.noreply.github.com>
2023-07-23 22:59:04 +0100

    Update analyze_data.py

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sql_db import read_data_from_database
+from sqlalchemy import create_engine
 
-def draw_data_grpahs(db_name):
+def read_data_from_database(db_name):
+    # Assuming you have already set up your database connection here
+    # Replace 'username', 'password', 'host', 'port', and 'database_name' with your database credentials
+    engine = create_engine(f"postgresql+psycopg2://username:password@host:port/database_name")
+    
+    # Assuming your table is named 'repositories' and has columns 'language', 'repository_count', and 'avg_stars'
+    query = f"SELECT * FROM repositories;"
+    
+    # Execute the SQL query and read the data into a Pandas DataFrame
+    df = pd.read_sql_query(query, engine)
+    
+    return df
+
+def draw_data_graphs(db_name):
     df = read_data_from_database(db_name)   
     # plotting the number of repositories per language
     plt.figure(figsize=(12, 6))
@@ -21,4 +34,8 @@ def draw_data_grpahs(db_name):
     plt.title('distribution of star counts per language')
     plt.xticks(range(1, len(df['language']) + 1), df['language'], rotation=90)
     plt.show()
+
+# Call the function with the appropriate database name
+draw_data_graphs('your_database_name')
+
 
