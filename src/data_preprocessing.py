98c07d663580094e510b2ed9eec37a39cf2c2242
satishreddyseelam <140163832+satishreddyseelam@users.noreply.github.com>
2023-07-23 23:04:25 +0100

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from data_aquisition import fetch_repository_data
 
-#pre processing the data
+# Preprocessing the data
 def preprocess_data(repository_data):
-    repositories = repository_data['items']
-    extracted_data = []
-    for repo in repositories:
-        extracted_data.append({
+    extracted_data = [
+        {
             'name': repo['name'],
             'language': repo['language'],
             'stars': repo['stargazers_count']
-        })
+        }
+        for repo in repository_data['items']
+    ]
     df = pd.DataFrame(extracted_data)
     return df
+
